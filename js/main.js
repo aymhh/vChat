ba9b7cb478045e875f3ba64ede119a867f158eed
aymhh <amr.alshmmaa@gmail.com>
2021-07-15 15:42:32 +1000

    fixed input validator

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
+function findName() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4) {
+            if(xhr.readyState === 200) {
+                console.log(xhr.responseText)
+            }
+
+            if(xhr.readyState === 404) {
+                console.log("THERE IS NOTHIGN HERE I CAN'T DISPLAY ANYTHING IF THERE IS NOTHING HERE 4000000000000004 ")
+            }
+        }
+    }
+
+    xhr.open('get', )
+}
+
+
+
+
 function logThisShit() {
     console.log(document.getElementById("pfp").value);
     console.log(document.getElementById("email").value);
     console.log(document.getElementById("uid").value);
     console.log(document.getElementById("pwd").value);
-    
+
 }
 
 
@@ -16,11 +37,11 @@ function loginValidator() {
     console.log(`${password.value} is the inputed value.`)
 
     submitButton.addEventListener('submit', function (e) {
-        if (username.value === "thisIsTheUsername" && password.value === "thisIsTheUsername") {
-            console.log("matching login Details entered!");
+        if (username.value === "thisIsTheUsername" && password.value === "thisIsThePassword") {
+            console.log("matching details entered!");
         } else {
             e.preventDefault();
-           alert('preventDefault');
+            alert('preventDefault');
         }
     })
 }
